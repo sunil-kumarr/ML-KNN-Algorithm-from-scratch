9e4d6f4a8b7fdc430ad9ba43a5059e5ef776ef19
sunil kumar <sunindus.sk@gmail.com>
2020-03-01 18:06:11 +0530

    completed knn function

--- knn_from_scratch.py
+++ knn_from_scratch.py
@@ -32,3 +32,58 @@ def knn_classifier(train_data_points, query_point, k_neighbors,distance_func,cho
     #!  mean function: KNN-Regressor
     #!  mode function : KNN-Classifier
     return k_nearest_neighbors,choice_func(k_nearest_neighbors_labels)
+
+def main():
+    '''
+    # Regression Data
+    #
+    # Column 0: height (inches)
+    # Column 1: weight (pounds)
+    '''
+    reg_data = [
+       [65.75, 112.99],
+       [71.52, 136.49],
+       [69.40, 153.03],
+       [68.22, 142.34],
+       [67.79, 144.30],
+       [68.70, 123.30],
+       [69.80, 141.49],
+       [70.01, 136.46],
+       [67.90, 112.37],
+       [66.49, 127.45],
+    ]
+
+    # Question:
+    # Given the data we have, what's the best-guess at someone's weight if they are 60 inches tall?
+    reg_query = [60]
+    reg_k_nearest_neighbors, reg_prediction = knn_classifier(
+        reg_data, reg_query, k_neighbors=3, distance_func=euclidian_distance_calc, choice_func=mean
+    )
+
+    '''
+    # Classification Data
+    #
+    # Column 0: age
+    # Column 1: likes pineapple
+    '''
+    clf_data = [
+       [22, 1],
+       [23, 1],
+       [21, 1],
+       [18, 1],
+       [19, 1],
+       [25, 0],
+       [27, 0],
+       [29, 0],
+       [31, 0],
+       [45, 0],
+    ]
+    # Question:
+    # Given the data we have, does a 33 year old like pineapples on their pizza?
+    clf_query = [33]
+    clf_k_nearest_neighbors, clf_prediction = knn_classifier(
+        clf_data, clf_query, k_neighbors=3, distance_func=euclidian_distance_calc, choice_func=mode
+    )
+
+if __name__ == '__main__':
+    main()
